9e67c9c7365c8a406be1a6077a04af09569bb054
Marco Montanari <mmontanari@labs.it>
2012-04-03 17:43:34 +0200

    tests

---
+++ meta_model/tests/test_rest_api.py
@@ -0,0 +1,81 @@
+import unittest
+
+class TestAttributeResource(unittest.TestCase):
+    def test_get_detail(self):
+        # attribute_resource = AttributeResource()
+        # self.assertEqual(expected, attribute_resource.get_detail(request, **kwargs))
+        assert False # TODO: implement your test here
+
+    def test_get_object_list(self):
+        # attribute_resource = AttributeResource()
+        # self.assertEqual(expected, attribute_resource.get_object_list(request))
+        assert False # TODO: implement your test here
+
+class TestBackendResource(unittest.TestCase):
+    def test_get_detail(self):
+        # backend_resource = BackendResource()
+        # self.assertEqual(expected, backend_resource.get_detail(request, **kwargs))
+        assert False # TODO: implement your test here
+
+    def test_get_object_list(self):
+        # backend_resource = BackendResource()
+        # self.assertEqual(expected, backend_resource.get_object_list(request))
+        assert False # TODO: implement your test here
+
+class TestDataSpaceResource(unittest.TestCase):
+    def test_get_detail(self):
+        # data_space_resource = DataSpaceResource()
+        # self.assertEqual(expected, data_space_resource.get_detail(request, **kwargs))
+        assert False # TODO: implement your test here
+
+    def test_get_object_list(self):
+        # data_space_resource = DataSpaceResource()
+        # self.assertEqual(expected, data_space_resource.get_object_list(request))
+        assert False # TODO: implement your test here
+
+class TestDataModelResource(unittest.TestCase):
+    def test_get_detail(self):
+        # data_model_resource = DataModelResource()
+        # self.assertEqual(expected, data_model_resource.get_detail(request, **kwargs))
+        assert False # TODO: implement your test here
+
+    def test_get_object_list(self):
+        # data_model_resource = DataModelResource()
+        # self.assertEqual(expected, data_model_resource.get_object_list(request))
+        assert False # TODO: implement your test here
+
+class TestFieldResource(unittest.TestCase):
+    def test_get_detail(self):
+        # field_resource = FieldResource()
+        # self.assertEqual(expected, field_resource.get_detail(request, **kwargs))
+        assert False # TODO: implement your test here
+
+    def test_get_object_list(self):
+        # field_resource = FieldResource()
+        # self.assertEqual(expected, field_resource.get_object_list(request))
+        assert False # TODO: implement your test here
+
+class TestPrivilegeResource(unittest.TestCase):
+    def test_get_detail(self):
+        # privilege_resource = PrivilegeResource()
+        # self.assertEqual(expected, privilege_resource.get_detail(request, **kwargs))
+        assert False # TODO: implement your test here
+
+    def test_get_object_list(self):
+        # privilege_resource = PrivilegeResource()
+        # self.assertEqual(expected, privilege_resource.get_object_list(request))
+        assert False # TODO: implement your test here
+
+class TestIndexResource(unittest.TestCase):
+    def test_get_detail(self):
+        # index_resource = IndexResource()
+        # self.assertEqual(expected, index_resource.get_detail(request, **kwargs))
+        assert False # TODO: implement your test here
+
+    def test_get_object_list(self):
+        # index_resource = IndexResource()
+        # self.assertEqual(expected, index_resource.get_object_list(request))
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
